6dd2befede7a8f9eaf6a9e2c1c15660624c92ff0
Vitor Gabriel <Ranbut_Beater@protonmail.com>
2023-06-01 17:56:33 -0300

    feat: Monsters List

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Home from './Home';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
+import Monsters from './Monsters';
 
-export { Home, SignIn, SignUp }
+export { Home, SignIn, SignUp, Monsters }
--- src/services/DnD Api/monstersApi.js
+++ src/services/DnD Api/monstersApi.js
@@ -2,5 +2,5 @@ import api from '../api';
 
 export async function getMonsters() {
   const response = await api.get('https://www.dnd5eapi.co/api/monsters');
-  return response.data;
+  return response.data.results;
 }
