a15c0b43c6342d6a85c3f3d034ca805ecf1481ad
Vitor Gabriel <Ranbut_Beater@protonmail.com>
2023-06-05 11:47:52 -0300

    feat: Part 2 - MonsterStats

--- src/components/MonsterStats/index.js
+++ src/components/MonsterStats/index.js
@@ -27,22 +27,38 @@ export default function MonsterStats({ monster }) {
 
     function calculateAbilityModifier(abilityScore) { return Math.floor((abilityScore - 10) / 2); };
 
+    function renderMonsterSavingThrows({ proficiencies }) {
+        const savingThrows = proficiencies.filter(item => item.proficiency.name.includes('Saving Throw'));
+
+        const savingThrowsString = savingThrows.map(item => `${item.proficiency.name.split(': ')[1]} +${item.value}`).join(', ');
+
+        return savingThrowsString;
+    }
+
+    function renderMonsterSkills({ proficiencies }) {
+        const skills = proficiencies.filter(item => item.proficiency.name.includes('Skill'));
+
+        const skillsString = skills.map(item => `${item.proficiency.name.split(': ')[1]} +${item.value}`).join(', ');
+
+        return skillsString;
+    }
+
     function renderMonsterImmunities({ damage_immunities }) {
         const capitalizeArray = damage_immunities.map(element =>
             element.charAt(0).toUpperCase() + element.slice(1)
-          );
-          const result = capitalizeArray.join(", ");
-          return result;
+        );
+        const result = capitalizeArray.join(", ");
+        return ' '+ result;
     }
 
     function renderMonsterSenses({ senses }) {
         function capitalizeFirstLetter(str) {
             return str.charAt(0).toUpperCase() + str.slice(1).replace(/_/g, " ");
-          }
+        }
 
-        return Object.entries(senses)
-        .map(([key, value]) => capitalizeFirstLetter(key) + " " + value)
-        .join(", ");
+        return ' ' + Object.entries(senses)
+            .map(([key, value]) => capitalizeFirstLetter(key) + " " + value)
+            .join(", ");
     }
 
     return (
@@ -66,11 +82,11 @@ export default function MonsterStats({ monster }) {
                         <StyledAtributteLabel>Armor class</StyledAtributteLabel>
                         <StyledAtributteValues>
                             <span className="mon-stat-block__attribute-data-value">
-                                19
+                                {monster.armor_class[0].value}
                             </span>
 
                             <span className="mon-stat-block__attribute-data-extra">
-                                (natural armor)
+                                ({monster.armor_class[0].type} armor)
                             </span>
                         </StyledAtributteValues>
                     </div>
@@ -150,24 +166,20 @@ export default function MonsterStats({ monster }) {
 
                     <div className="mon-stat-block__tidbit">
                         <StyledTidbitLabel>Saving Throws</StyledTidbitLabel>
-                        <span className="mon-stat-block__tidbit-data">
-                            DEX +7, CON +10, WIS +6, CHA +8
-                        </span>
+                        <span className="mon-stat-block__tidbit-data"> {renderMonsterSavingThrows(monster)}</span>
                     </div>
 
                     <div className="mon-stat-block__tidbit">
                         <StyledTidbitLabel>Skills</StyledTidbitLabel>
-                        <span className="mon-stat-block__tidbit-data">
-                            <a className="tooltip-hover skill-tooltip" href="/compendium/rules/basic-rules/using-ability-scores#Perception" data-tooltip-href="//www.dndbeyond.com/skills/14-tooltip?disable-webm=1&amp;disable-webm=1">Perception</a> +11, <a className="tooltip-hover skill-tooltip" href="/compendium/rules/basic-rules/using-ability-scores#Stealth" data-tooltip-href="//www.dndbeyond.com/skills/5-tooltip?disable-webm=1&amp;disable-webm=1">Stealth</a> +7
-                        </span>
+                        <span className="mon-stat-block__tidbit-data"> {renderMonsterSkills(monster)}</span>
                     </div>
-                    {monster.damage_immunities.length > 0 ? (                    
-                    <div className="mon-stat-block__tidbit">
-                        <StyledTidbitLabel>Damage Immunities</StyledTidbitLabel>
-                        <span className="mon-stat-block__tidbit-data">
-                            {renderMonsterImmunities(monster)}
-                        </span>
-                    </div>) : (<></>)}
+                    {monster.damage_immunities.length > 0 ? (
+                        <div className="mon-stat-block__tidbit">
+                            <StyledTidbitLabel>Damage Immunities</StyledTidbitLabel>
+                            <span className="mon-stat-block__tidbit-data">
+                                {renderMonsterImmunities(monster)}
+                            </span>
+                        </div>) : (<></>)}
                     <div className="mon-stat-block__tidbit">
                         <StyledTidbitLabel>Senses</StyledTidbitLabel>
                         <span className="mon-stat-block__tidbit-data">
@@ -177,19 +189,14 @@ export default function MonsterStats({ monster }) {
 
                     <div className="mon-stat-block__tidbit">
                         <StyledTidbitLabel>Languages</StyledTidbitLabel>
-                        <span className="mon-stat-block__tidbit-data">
-                            {monster.languages}
-                        </span>
+                        <span className="mon-stat-block__tidbit-data"> {monster.languages}</span>
                     </div>
 
                     <div className="mon-stat-block__tidbit-container">
                         <div className="mon-stat-block__tidbit">
                             <StyledTidbitLabel>Challenge</StyledTidbitLabel>
-                            <span className="mon-stat-block__tidbit-data">
-                                {monster.challenge_rating} ({monster.xp.toLocaleString('en-US')} XP)
-                            </span>
+                            <span className="mon-stat-block__tidbit-data"> {monster.challenge_rating} ({monster.xp.toLocaleString('en-US')} XP)</span>
                         </div>
-
                         <div className="mon-stat-block__tidbit-spacer"></div>
                         <div className="mon-stat-block__tidbit">
                             <StyledTidbitLabel>Proficiency Bonus</StyledTidbitLabel >
@@ -208,30 +215,26 @@ export default function MonsterStats({ monster }) {
 
                     <div className="mon-stat-block__description-block">
                         <div className="mon-stat-block__description-block-content">
-                            <p><em><strong>Amphibious.</strong></em> The dragon can breathe air and water.</p>
-                            <p><em><strong>Legendary Resistance (3/Day).</strong></em> If the dragon fails a saving throw, it can choose to succeed instead.</p>
+                            {monster.special_abilities.map((ability, index) => (
+                                <p key={index}><em><strong>{ability.name} {ability.usage && ability.usage.times ? `(${ability.usage.times}/Day)` : ''}.</strong></em> {ability.desc}</p>
+                            ))}
                         </div>
                     </div>
 
                     <div className="mon-stat-block__description-block">
                         <div className="mon-stat-block__description-block-heading">Actions</div>
                         <div className="mon-stat-block__description-block-content">
-                            <p><em><strong>Multiattack.</strong></em> The dragon can use its Frightful Presence. It then makes three attacks: one with its bite and two with its claws.</p>
-                            <p><em><strong>Bite.</strong> Melee Weapon Attack:</em> <span data-dicenotation="1d20+11" data-rolltype="to hit" data-rollaction="Bite">+11</span> to hit, reach 10 ft., one target. <em>Hit:</em> 17 <span data-dicenotation="2d10+6" data-rolltype="damage" data-rollaction="Bite" data-rolldamagetype="piercing">(2d10 + 6)</span> piercing damage plus 4 <span data-dicenotation="1d8" data-rolltype="damage" data-rollaction="Bite" data-rolldamagetype="acid">(1d8)</span> acid damage.</p>
-                            <p><em><strong>Claw.</strong> Melee Weapon Attack:</em> <span data-dicenotation="1d20+11" data-rolltype="to hit" data-rollaction="Claw">+11</span> to hit, reach 5 ft., one target. <em>Hit:</em> 13 <span data-dicenotation="2d6+6" data-rolltype="damage" data-rollaction="Claw" data-rolldamagetype="slashing">(2d6 + 6)</span> slashing damage.</p>
-                            <p><em><strong>Tail.</strong> Melee Weapon Attack:</em> <span data-dicenotation="1d20+11" data-rolltype="to hit" data-rollaction="Tail">+11</span> to hit, reach 15 ft., one target. <em>Hit:</em> 15 <span data-dicenotation="2d8+6" data-rolltype="damage" data-rollaction="Tail" data-rolldamagetype="bludgeoning">(2d8 + 6)</span> bludgeoning damage.</p>
-                            <p><em><strong>Frightful Presence.</strong></em> Each creature of the dragon's choice that is within 120 feet of the dragon and aware of it must succeed on a DC 16 Wisdom saving throw or become <a className="tooltip-hover condition-tooltip" href="/compendium/rules/basic-rules/appendix-a-conditions#Frightened" data-tooltip-href="//www.dndbeyond.com/conditions/5-tooltip?disable-webm=1&amp;disable-webm=1">frightened</a> for 1 minute. A creature can repeat the saving throw at the end of each of its turns, ending the effect on itself on a success. If a creature's saving throw is successful or the effect ends for it, the creature is immune to the dragon's Frightful Presence for the next 24 hours.</p>
-                            <p><em><strong>Acid Breath <span data-dicenotation="1d6" data-rolltype="recharge" data-rollaction="Acid Breath">(Recharge 5–6)</span>.</strong></em> The dragon exhales acid in a 60-­foot line that is 5 feet wide. Each creature in that line must make a DC 18 Dexterity saving throw, taking 54 <span data-dicenotation="12d8" data-rolltype="damage" data-rollaction="Acid Breath" data-rolldamagetype="acid">(12d8)</span> acid damage on a failed save, or half as much damage on a successful one.</p>
+                        {monster.actions.map((action, index) => (
+                            <p key={index}><em><strong>{action.name}{action.usage && action.usage.min_value ? ` (Recharge ${action.usage.min_value}–6)` : ''}{action.usage && action.usage.times ? ` (${action.usage.times}/Day)` : ''}.</strong> {action.desc}</em></p>
+                            ))}
                         </div>
                     </div>
-
                     <div className="mon-stat-block__description-block">
                         <div className="mon-stat-block__description-block-heading">Legendary Actions</div>
                         <div className="mon-stat-block__description-block-content">
-                            <p>The dragon can take 3 legendary actions, choosing from the options below. Only one legendary action option can be used at a time and only at the end of another creature's turn. The dragon regains spent legendary actions at the start of its turn.</p>
-                            <p><strong>Detect.</strong> The dragon makes a Wisdom (<a className="tooltip-hover skill-tooltip" href="/compendium/rules/basic-rules/using-ability-scores#Perception" data-tooltip-href="//www.dndbeyond.com/skills/14-tooltip?disable-webm=1&amp;disable-webm=1">Perception</a>) check.</p>
-                            <p><strong>Tail Attack.</strong> The dragon makes a tail attack.</p>
-                            <p><strong>Wing Attack (Costs 2 Actions).</strong> The dragon beats its wings. Each creature within 10 feet of the dragon must succeed on a DC 19 Dexterity saving throw or take 13 <span data-dicenotation="2d6+6" data-rolltype="damage" data-rollaction="Wing Attack" data-rolldamagetype="bludgeoning">(2d6 + 6)</span> bludgeoning damage and be knocked <a className="tooltip-hover condition-tooltip" href="/compendium/rules/basic-rules/appendix-a-conditions#Prone" data-tooltip-href="//www.dndbeyond.com/conditions/12-tooltip?disable-webm=1&amp;disable-webm=1">prone</a>. The dragon can then fly up to half its flying speed.</p>
+                        {monster.legendary_actions.map((action, index) => (
+                            <p key={index}><em><strong>{action.name}.</strong> {action.desc}</em></p>
+                            ))}
                         </div>
                     </div>
 
